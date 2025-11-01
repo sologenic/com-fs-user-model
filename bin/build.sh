cfdb0d24f09a3b4e6054f0f705e8eb80ff6a150c
Norbert van Nobelen <norbert.van.nobelen2@gmail.com>
2025-11-01 09:39:14 -0700

    fix: Remove unwanted typescript builds for -grpc files

--- bin/build.sh
+++ bin/build.sh
@@ -83,22 +83,6 @@ protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
     --ts_proto_opt=esModuleInterop=true \
     user-filters.proto
 
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
-    --proto_path=. \
-    --proto_path=$(dirname $(dirname "$rd")) \
-    --ts_proto_out=. \
-    --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_opt=outputServices=grpc-js \
-    user-grpc.proto
-
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
-    --proto_path=. \
-    --proto_path=$(dirname $(dirname "$rd")) \
-    --ts_proto_out=. \
-    --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_opt=outputServices=grpc-js \
-    adminuser-grpc.proto
-
 npm run build
 git add build/
 
